d6056f363f370cc103bcf8b86df4d96647856f73
Bogdan Mocanu <b.mocanu@student.utwente.nl>
2024-04-12 14:05:04 +0200

    Implement the RoutingInfo

--- src/RoutingInfo.java
+++ src/RoutingInfo.java
@@ -1,2 +1,24 @@
 public class RoutingInfo {
+    private String username;
+    private int destination;
+    private int nextHop;
+
+
+    public RoutingInfo(int destination, int nextHop, String username) {
+        this.destination = destination;
+        this.nextHop = nextHop;
+        this.username = username;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    public int getNextHop() {
+        return nextHop;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
